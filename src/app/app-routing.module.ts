3e2c6374bab4eccf2b29165a20d5163cc0e06ed1
nguyen thanh dat <thagdet23@gmail.com>
2018-12-26 18:00:24 +0700

    ;

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {SubjectComponent} from './subject/subject.component';
+import {CreateTestComponent} from './create-test/create-test.component';
 
 const routes: Routes = [
   { path: 'subject', component: SubjectComponent },
