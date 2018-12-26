ed406004257e2680ca5f0f1f4239e53538a3d03f
nguyen thanh dat <thagdet23@gmail.com>
2018-12-26 17:55:43 +0700

    fa

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {SubjectComponent} from './subject/subject.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'subject', component: SubjectComponent }
+  ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,12 +10,14 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LoginService } from './_services/login.service';
+import { SubjectComponent } from './subject/subject.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    LoginComponent
+    LoginComponent,
+    SubjectComponent
   ],
   imports: [
     BrowserModule,
