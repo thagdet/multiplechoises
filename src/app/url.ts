4d9f2ef39672ee831a253b7afead7a088727df2c
nguyen thanh dat <thagdet23@gmail.com>
2019-01-15 22:42:06 +0700

    fix

--- src/app/url.ts
+++ src/app/url.ts
@@ -4,9 +4,4 @@ import {Injectable} from '@angular/core';
 export class Url {
   public static URL = 'https://evening-sierra-59961.herokuapp.com/';
 }
-export const enum AUTH {
-  AUTH_OK,
-  UN_AUTHENTICATED,
-  UN_AUTHORIZED,
-}
 
