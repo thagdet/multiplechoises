c78319a53acc8efeb174d0a33a14ea532a0ea6ae
nguyen thanh dat <thagdet23@gmail.com>
2019-01-15 13:48:44 +0700

    index

--- src/app/server.js
+++ src/app/server.js
@@ -5,11 +5,11 @@ var path = require('path');
 var app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/MultiplechoicesManagement'));
+app.use(express.static('/dist/MultiplechoicesManagement'));
 
 app.get('/*', function(req,res) {
 
-  res.sendFile(path.join(__dirname+'/dist/MultiplechoicesManagement/index.html'));
+  res.sendFile(path.join('/dist/MultiplechoicesManagement/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
